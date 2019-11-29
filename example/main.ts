b9bd8205579b46a85cd85292684e225d2dcfff3e
Carlos Fdez. Llamas <hello@sirikon.me>
2019-11-29 22:48:23 +0100

    Configuration.

--- example/main.ts
+++ example/main.ts
@@ -1,3 +1,16 @@
+import * as pathUtils from 'path';
+
 import { Gratin } from '..';
 
-const cosa = new Gratin();
+const gratin = new Gratin({
+	migrationsFolder: pathUtils.join(__dirname, 'migrations'),
+	database: {
+		address: 'localhost',
+		port: '5432',
+		username: 'postgres',
+		password: '12345',
+		schema: 'postgres',
+	},
+});
+
+gratin.run();
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
+export interface IGratinConfiguration {
+	migrationsFolder: string;
+	database: {
+		address: string;
+		port: string;
+		username: string;
+		password: string;
+		schema: string;
+	};
+}
+
 export class Gratin {
-	constructor() {
+
+	constructor(
+		private configuration: IGratinConfiguration) { }
+
+	public run() {
 		// tslint:disable-next-line: no-console
-		console.log('Hello World!');
+		console.log(this.configuration);
 	}
+
 }
